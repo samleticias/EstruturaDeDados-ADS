2a463b4486e7e2ffb06cb20c7f74b15dcaa484e1
samleticias <sammyaleticia09@gmail.com>
2024-11-03 22:41:41 -0300

    lista encadeada

--- Listas/main2.c
+++ Listas/main2.c
@@ -6,57 +6,70 @@ typedef struct no {
     struct no *proximo;
 } No;
 
+typedef struct {
+    No *inicio;
+    int tam;
+} Lista;
+
+void criarLista(Lista *lista){
+    lista->inicio = NULL;
+    lista->tam = 0;
+}
+
 // inserir no inicio da lista
-void inserirNoInicio(No **lista, int num){
+void inserirNoInicio(Lista *lista, int num){
     No *novo = malloc(sizeof(No)); // aloca memoria para novo nó
     if (novo){
         novo->valor = num;
-        novo->proximo = *lista; // novo nó aponta para inicio da lista
-        *lista = novo; // atualiza inicio da lista que aponta para o novo nó
+        novo->proximo = lista->inicio; // novo nó aponta para inicio da lista
+        lista->inicio = novo; // atualiza inicio da lista que aponta para o novo nó
+        lista->tam++; // incrementa tamanho da lista a cada novo elemento inserido
     } else {
         printf("\nerro ao alocar memoria!\n");
     }
 }
 
 // inserir no final da lista
-void inserirNoFinal(No **lista, int num){
+void inserirNoFinal(Lista *lista, int num){
     No *atual, *novo = malloc(sizeof(No)); // aloca memoria para novo nó
     if (novo){
         novo->valor = num;
         novo->proximo = NULL;
         // se a lista está vazia, o novo nó se torna o primeiro nó
-        if (*lista == NULL){
-            *lista = novo;
+        if (lista->inicio == NULL){
+            lista->inicio = novo;
         } else {
             // ponteiro atual aponta para o inicio da lista
-            atual = *lista;
+            atual = lista->inicio;
             
             // loop percorre toda a lista até que o proximo aponte para null para encontrar o final da lista
             while (atual->proximo != NULL){
                 // a cada volta, atualiza o atual para ser o proximo, percorrendo todos os elementos da lista
                 atual = atual->proximo; 
             }
+
             // loop para quando o proximo apontar para null e atualiza o atual para ser o novo, encadeando/inserindo elemento ao final
             // da lista
             atual->proximo = novo;
         }
+        lista->tam++;
     } else {
         printf("\nerro ao alocar memoria!\n");
     }
 }
 
-// inserir no meio da lista
-void inserirNoMeio(No **lista, int num, int ant){
+void inserirNoMeio(Lista *lista, int num, int ant){
     No *atual, *novo = malloc(sizeof(No));
     if (novo){
         novo->valor = num;
+
         // se a lista está vazia, o novo nó se torna o primeiro nó
-        if (*lista == NULL){
+        if (lista->inicio == NULL){
             novo->proximo = NULL;
-            *lista = novo;
+            lista->inicio = novo;
         } else {
             // variavel auxiliar para armazenar inicio da lista
-            atual = *lista;
+            atual = lista->inicio;
             // loop ocorre enquanto o valor do atual for diferente do valor anterior (valor de referencia) e proximo nao for null
             while(atual->valor != ant && atual->proximo != NULL){
                 atual = atual->proximo; // atualiza variavel auxiliar para apontar para o proximo elemento da lista
@@ -65,45 +78,22 @@ void inserirNoMeio(No **lista, int num, int ant){
             // proximo de 2 (novo elemento) para apontar para o proximo de 1 (que é 3) 
             // 2->proximo = 1->proximo (que é 3)
             novo->proximo = atual->proximo;
+
             // ponteiro proximo do atual vai apontar para o novo elemento a ser inserido no meio
             // 1-> proximo = 2 (que é o novo)
             atual->proximo = novo;
         }
+        lista->tam++;
     } else {
         printf("\nerro ao alocar memoria!\n");
     }
 }
 
-void inserirOrdenado(No **lista, int num){
-    No *aux, *novo = malloc(sizeof(No));
-    if (novo){
-        novo->valor = num;
-        // se a lista está vazia
-        if (*lista == NULL){
-            novo->proximo = NULL;
-            *lista = novo;
-        // novo elemento é o menor
-        } else if (novo->valor < (*lista)->valor){
-            novo->proximo = *lista;
-            *lista = novo;
-        } else {
-            aux = *lista;
-            // encerra quando proximo é nulo ou o novo valor nao é maior que o valor do proximo nó
-            while(aux->proximo && novo->valor > aux->proximo->valor){
-                aux = aux->proximo;
-            }
-            novo->proximo = aux->proximo;
-            aux->proximo = novo;
-        }
-    } else {
-        printf("\nerro ao alocar memoria!\n");
-    }
-}
-
-// imprimir lista encadeada na tela
-void imprimir(No **lista){
-    No *no = *lista; 
-    printf("\nlista encadeada: ");
+void imprimir(Lista lista){
+    // elemento struct usa ponto
+    // ponteiro usa seta
+    No *no = lista.inicio; // copia da lista
+    printf("\nlista encadeada tam %d: ", lista.tam);
     while(no != NULL){
         printf("%d ", no->valor);
         no = no->proximo;
@@ -113,10 +103,13 @@ void imprimir(No **lista){
 
 int main(){
     int opcao, valor, anterior;
-    No *lista = NULL;
+    // No *lista = NULL;
+    Lista lista;
+
+    criarLista(&lista);
 
     do {
-        printf("\n\t1 - inserir no inicio\n\t2 - inserir no final\n\t3 - inserir no meio\n\t4 - imprimir lista\n\t5 - inserir ordenado\n\t0 - sair\n\t> ");
+        printf("\n\t1 - inserir no inicio\n\t2 - inserir no final\n\t3 - inserir no meio\n\t4 - imprimir lista\n\t0 - sair\n\t> ");
         scanf("%d", &opcao);
 
         switch (opcao){
@@ -136,12 +129,7 @@ int main(){
                 inserirNoMeio(&lista, valor, anterior);
                 break;
             case 4:
-                imprimir(&lista);
-                break;
-            case 5:
-                printf("\ndigite um valor: ");
-                scanf("%d", &valor);
-                inserirOrdenado(&lista, valor);
+                imprimir(lista);
                 break;
             default:
                 if (opcao != 0){
@@ -152,4 +140,5 @@ int main(){
     } while(opcao != 0);
 
     return 0;
+    
 }
